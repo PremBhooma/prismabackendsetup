51c85aa5bffaaaa11f37438ae5e5318cba810036
PremBhooma <bhoomasagar1213@gmail.com>
2025-02-13 13:14:52 +0530

    `Added CORS middleware and updated imports`

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require("express");
 require("dotenv").config();
+const cors = require("cors");
 const path = require("path");
 
 const app = express();
 app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 const authRouter = require("./routes/authRoute.js");
